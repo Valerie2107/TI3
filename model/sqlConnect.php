7fa1f4ff81672701661f3a902f53465bca4ed08d
valerie2107 <kinkamyl@hotmail.com>
2023-02-22 18:28:08 +0100

    connexion DB

--- model/sqlConnect.php
+++ model/sqlConnect.php
@@ -1,1 +1,24 @@
-connexion à la DB ti3
+<?php
+//connexion à la DB 
+// les Paramètres de connexion sont dans config
+
+
+
+try {
+    // Création de la connexion
+    $conn = mysqli_connect(DB_HOST, DB_LOGIN, DB_PWD, DB_NAME, DB_PORT, DB_CHARSET);
+    mysqli_set_charset($conn,DB_CHARSET);
+    
+    // Vérification de la connexion
+    if (!$conn) {
+        throw new Exception("La connexion à la base de données a échoué.");
+    }
+    //var_dump($conn);
+    echo "Connected successfully";
+} catch (Exception $e) {
+    // Gestion des erreurs
+    die("Erreur : " . $e->getMessage());
+}
+
+
+?>
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+//paramètre sécurité
+require_once "../config.php";
+
+//connexion DB  --> faut-il once ou pas ?
+require_once "../model/sqlConnect.php";
+
+
+
+?>
