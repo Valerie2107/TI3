92d13b2fa766b13ac255e8914c89b86f38654511
valerie2107 <kinkamyl@hotmail.com>
2023-02-24 15:28:52 +0100

    nav, sitch et pages

--- public/index.php
+++ public/index.php
@@ -8,4 +8,39 @@ require_once "../model/sqlConnect.php";
 
 
 
+// mon controleur frontal
+if(isset($_GET['vue'])){
+
+    switch($_GET['vue']){
+        case 'accueil':
+            include "views/accueil.php";
+            break;
+        case 'admin':
+            include "views/admin.php";
+            break;
+        case 'contact':
+            include "views/contact.php";
+            break;
+        case 'cv':
+            include "views/cv.php";
+            break;
+        case 'liens':
+            include "views/liens.php";
+            break;
+        case 'projets':
+            include "views/projets.php";
+            break;
+        case 'travaux':
+                include "views/travaux.php";
+                break;
+        case 'tutos':
+            include "views/tutos.php";
+            break;
+        default:
+            include_once "views/accueil.php";
+    }
+}else{
+        include_once "views/accueil.php";
+    }
+
 ?>
---
+++ views/include/nav.inc.php
@@ -0,0 +1,10 @@
+<nav>
+    <a href="?vue=accueil">Accueil</a>
+    <a href="?vue=admin">Admin</a>
+    <a href="?vue=cv">Curriculum</a>
+    <a href="?vue=liens">Liens</a>
+    <a href="?vue=contact">Contact</a>
+    <a href="?vue=projets">Projets</a>
+    <a href="?vue=travaux">Travaux</a>
+    <a href="?vue=tutos">Tutos</a>
+</nav>
