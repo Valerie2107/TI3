90751a361b0166453983fd06780b9b316a458132
valerie2107 <kinkamyl@hotmail.com>
2023-02-27 21:48:59 +0100

    dernier de la journée, link css ok

--- views/include/footer.inc.php
+++ views/include/footer.inc.php
@@ -1,7 +1,8 @@
 
 
 <footer>
-
+    <p><a href="?views=contact">lien vers contact</a></p>
+    <br>
     <p><a href="?views=admin">lien vers la page admin</a></p>
     <button onclick="backToTop()" id="btt" title="Back to top">Top</button> 
 
