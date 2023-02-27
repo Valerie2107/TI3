9bea418540e3a453ae6ff48e0d6250bcecb7c6e2
valerie2107 <kinkamyl@hotmail.com>
2023-02-27 21:33:18 +0100

    fin formulaire, mais sans CSS

--- views/contact.php
+++ views/contact.php
@@ -13,19 +13,17 @@
         ?>
     </header>
     <main>
-        <form action="" method="post" name="">
-            <label for="nom">
-                <input type="text">
-            </label>
-            <label for="prénom">
-                <input type="text">
-            </label>
-            <label for="adresse e-mail">
-                <input type="email">
-            </label>
-            <textarea name="" id="" cols="30" rows="10">
-                
-            </textarea>
+        <form method="post" action="" name="form1">
+            <!--champ de texte avec son label. Le for du label doit avoir la même valeur que l'id de l'élément du formulaire, ne pas faire un for "name" pour un id "mail" -->
+            <label for="nom">Nom: </label><input type="text" name="nom" id="nom" placeholder="Entrez ici votre nom"> 
+            <br>
+            <label for="prenom">Prénom: </label><input type="text" name="prenom" id="prenom" placeholder="Entrez ici votre prénom">
+            <br>
+            <label for="mail">E-mail: </label><input type="email" name="mail" id="mail" placeholder="entrez votre adresse e-mail">
+            <br>
+            <textarea name="" id="" cols="80" rows="10" placeholder="Raison de votre message"></textarea>
+            <br>
+            <input type="submit" value="Envoyer"> 
         </form>
     </main>
    
