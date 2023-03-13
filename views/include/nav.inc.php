fb385d1886dc3697199c81d555b445f57f1aa9c5
valerie2107 <kinkamyl@hotmail.com>
2023-03-13 09:36:45 +0100

    nav et eam

--- views/include/nav.inc.php
+++ views/include/nav.inc.php
@@ -2,12 +2,10 @@
   <nav>
     <a href="?views=accueil">Accueil</a>
     <a href="?views=projets">Projets</a>
-    <!--  <a href="?views=eam">Éducation Aux Médias</a>  -->
     <a href="?views=tutos">Tutoriels</a>
     <a href="?views=liens">Liens</a>
     <a href="?views=cv">Curriculum</a>
     <a href="?views=contact">Contact</a>
-    <br>
     <a href="?views=admin">Admin</a>
   </nav>
 </div>
