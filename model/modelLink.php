9f9df4d7a41e7a6124d69b8a9ab0079c2fd2de87
valerie2107 <kinkamyl@hotmail.com>
2023-03-14 11:37:01 +0100

    liens avec database

--- model/modelLink.php
+++ model/modelLink.php
@@ -6,15 +6,8 @@ function dataLien($uneDb){
     /* requête SQL, recuperation donnée Table DB*/
     $dataLinksql="SELECT * FROM `liens`";
 
-    try{
-
         /*Variable connexion DB + recuperation requête SQL*/
-        $querydataLink=mysqli_query($uneDb, $dataLinksql);
-    }catch(Exception $e){
-
-        die($e -> getMessage());
-
-    }
+    $querydataLink=mysqli_query($uneDb, $dataLinksql);
 
         /*recuperation requête en table + conversion tableau assiociatif*/
     return mysqli_fetch_all($querydataLink, MYSQLI_ASSOC);
--- views/liens.php
+++ views/liens.php
@@ -3,8 +3,10 @@
 <head>
     <?php include_once "include/avg_head.inc.php";  ?>
 
-    <title>Administration</title></head>
+    <title>Administration</title>
+</head>
 <body>
+   
     <header>
         <h1>Liens Utiles</h1>
         <?php include "../views/include/header.inc.php"; ?>
@@ -13,18 +15,20 @@
         J’ai évidemment suivit des tutos en plus des cours. Voici une liste non exhaustive des sites, blogs et autres chaînes YouTube que j’ai consultés afin de m’améliorer.
         <div class="empty"></div>
         <?php
-        foreach ($dataLink as $contenu)
-    ?>
+        foreach ($dataLink as $contenu):?>
   
-    <p><?=['titre']?></p>
+    <p><?=$contenu['titre']?></p>
     <br>
 
     <p><?=$contenu['description']?> </p>
+    <br>
+    <p>
+        <a href="<?=$contenu['url']?>"><?=$contenu['url']?> </a></p>
     </main>
     
+    <?php endforeach; ?>
     <?php 
         include "../views/include/footer.inc.php";
     ?>
-    
 </body>
 </html>
