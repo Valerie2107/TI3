540c603b999e19a97b902d448387ffc7d27c68d8
valerie2107 <kinkamyl@hotmail.com>
2023-03-14 15:57:49 +0100

    span page accueil

--- views/accueil.php
+++ views/accueil.php
@@ -16,28 +16,25 @@
             <span id="myself">Valérie VH <br>
             Portfolio</span></p> 
              <div id="accordeon-container">
-                    <div class="accordeon-header">
-                        Qui suis-je ?
-                    </div>
+                <div class="accordeon-header">
+                    Qui suis-je ?
+                </div>
                     <div class="accordeon-content">Jeune web-dev toujours en cours de formation, je suis quelqu'un de positif et dynamique.</div>
-                    <div class="accordeon-header">
-                       Pourquoi m'engager ?
-                    </div>
-                    <div class="accordeon-content">Jamais à l'abri d'un jeu de mot facile, je suis surtout quelqu'un de sérieux et d'investi dans mon travail.</div>
-                    <div class="accordeon-header">
-                        Quel est mon parcours ?
-                    </div>
-                    <div class="accordeon-content">Si mon revirement vers le web-dev est récent (après mûre réflexion), mon parcours professionnel commence assez tôt dans tout ce qui est contact clients. 
-                        J'ai donc beaucoup d'expériences professionnelles tant en contact direct que par call-center et comme gestionnaire de dossiers. Je me ferai un plaisir de vous conter cela lors d'un entretien.</div>
-                    <div class="accordeon-header">
-                        Comment me contacter ?
-                    </div>
+                <div class="accordeon-header">
+                    Pourquoi m'engager ?
+                </div>
+                    <div class="accordeon-content">Jamais à l'abri d'un jeu de mot facile, je suis avant tout quelqu'un de sérieux et d'investi dans son travail.</div>
+                <div class="accordeon-header">
+                    Quel est mon parcours ?
+                </div>
+                    <div class="accordeon-content">Si mon revirement vers le web-dev est récent bien que mûrement réfléchi, mon parcours professionnel commence assez tôt  et touche à tout ce qui est contact clients. <br>
+                        J'ai donc beaucoup d'expériences professionnelles tant en contact direct que par call-center et comme gestionnaire de dossiers. J'ai détaillé un peu plus mes expériences dans la section curriculum, et je me ferai un plaisir de vous conter cela lors d'un entretien.</div>
+                <div class="accordeon-header">
+                    Comment me contacter ?
+                </div>
                     <div class="accordeon-content">En suivant <a href="?views=contact">ce lien</a> ou en cliquant sur le bouton contact en bas de page.</div>
-                </div>    
-            </p>
-          
-               
-        
+               </div>    
+        </p>
     </main>
    
 
